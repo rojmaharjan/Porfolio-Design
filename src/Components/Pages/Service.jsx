9876c4e7ae596de8f76d12b8b2db57e89b4a8a94
rojmaharjan <mhjroj78@gmail.com>
2024-12-11 16:42:07 +0545

    commit

--- src/Components/Pages/Service.jsx
+++ src/Components/Pages/Service.jsx
@@ -25,7 +25,7 @@ const Service = () => {
               </div>
               <h2>E-Voting System</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nulla optio ipsa corrupti! Ad neque consectetur explicabo ipsam eos amet?
+              An E-voting system is a digital platform that allows eligible voters to cast their votes electronically in elections.E-voting systems aim to increase accessibility, reduce fraud election and process by providing a more convenient and faster alternative to traditional paper-based voting.
               </p>
               <p><a href="https://electronics-voting-system.vercel.app/" style={{color:'rgb(88, 88, 238)'}}>Click here to visit the site</a></p>
             </div>
@@ -35,7 +35,7 @@ const Service = () => {
               </div>
               <h2>Order Up</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nulla optio ipsa corrupti! Ad neque consectetur explicabo ipsam eos amet?
+              A Order Up System is an online platform that enables users to order food from restaurants and delivered to their desired location.Customers can browse menus, customize their orders, and make payments digitally through the system.It is only a frontend website that i build.It's not completed yet.
               </p>
               <p><a href="https://frontend-food-delivery-iota.vercel.app/" style={{color:'rgb(88, 88, 238)'}}>Click here to visit the site</a></p>
             </div>
@@ -45,7 +45,7 @@ const Service = () => {
               </div>
               <h2>Recipe Info </h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nulla optio ipsa corrupti! Ad neque consectetur explicabo ipsam eos amet?
+              It is a user interface for adding a new recipe in a Recipe Information System.It includes input fields for the recipe name, ingredients, and a description of the recipe. It's a responsive and user-friendly, with buttons to add recipe, edit and delete to make it easy to add and manage recipes.
               </p>
               <p><a href="https://recipe-food-info-display.vercel.app/" style={{color:'rgb(88, 88, 238)'}}>Click here to visit the site</a></p>
             </div>
@@ -55,7 +55,7 @@ const Service = () => {
               </div>
               <h2>Counter System</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nulla optio ipsa corrupti! Ad neque consectetur explicabo ipsam eos amet?
+              A Counter Application is a simple digital tool used to increment, decrement, or reset a numerical count and with also time counting.  It typically features buttons for increasing or decreasing the count and a reset button to return the count to zero and start and stop the time.
               </p>
               <p><a href="https://counter-application-rho.vercel.app/" style={{color:'rgb(88, 88, 238)'}}>Click here to visit the site</a></p>
             </div>
